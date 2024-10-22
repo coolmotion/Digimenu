d8f93dcae46399fd33c61205e29783807a2ce932
coolmotion <hcnpeiris@gmail.com>
2024-10-22 21:53:37 +0530

    product info

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -2,5 +2,8 @@ from main.models import Profile
 from django.shortcuts import get_object_or_404
 
 def profile(request):
-    profile = get_object_or_404(Profile, user=request.user)
-    return { "profile":profile}
+    if request.user.is_authenticated:
+        profile = get_object_or_404(Profile, user=request.user)
+        return {"profile": profile}
+    else:
+        return {}  # Return an empty context or default values as needed
